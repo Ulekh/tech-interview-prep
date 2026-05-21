164ee851b34ad50910f7d270c05278b2a2d4af0c
Oleg Bordun <oleg.bordun@outlook.com>
2026-05-21 11:35:18 +0200

    refactor(array-prototype-filter): clean up code style and remove commented implementation in myFilter

--- exercises/typescript/array-prototype-filter/index.test.ts
+++ exercises/typescript/array-prototype-filter/index.test.ts
@@ -1,4 +1,4 @@
-import "./index";
+import './index';
 
 const isEven = (element: any, index: number) => element % 2 === 0;
 const isOdd = (element: any, index: number) => element % 2 === 1;
@@ -11,58 +11,49 @@ const isSquareOdd = (_: any, index: number, array: Array<any>) =>
 const isThisProductEven = function (this: any, element: number) {
   return (element * this) % 2 === 0;
 };
-const isThisProductEvenArrowFn = (element: number) =>
-  (element * this!) % 2 === 0;
+const isThisProductEvenArrowFn = (element: number) => (element * this!) % 2 === 0;
 
-describe("Array.prototype.myFilter", () => {
-  test("empty array", () => {
+describe('Array.prototype.myFilter', () => {
+  test('empty array', () => {
     expect([].myFilter(isEven)).toStrictEqual([]);
     expect([].myFilter(isOdd)).toStrictEqual([]);
   });
 
-  test("one value", () => {
+  test('one value', () => {
     expect([1].myFilter(isEven)).toStrictEqual([]);
     expect([1].myFilter(isOdd)).toStrictEqual([1]);
   });
 
-  test("two values", () => {
+  test('two values', () => {
     expect([1, 10].myFilter(isEven)).toStrictEqual([10]);
     expect([1, 10].myFilter(isOdd)).toStrictEqual([1]);
   });
 
-  test("multiple values", () => {
+  test('multiple values', () => {
     expect([1, 2, 3, 5, 7, 8, 9].myFilter(isEven)).toStrictEqual([2, 8]);
-    expect([1, 2, 3, 5, 7, 8, 9].myFilter(isOdd)).toStrictEqual([
-      1, 3, 5, 7, 9,
-    ]);
+    expect([1, 2, 3, 5, 7, 8, 9].myFilter(isOdd)).toStrictEqual([1, 3, 5, 7, 9]);
   });
 
-  test("reducer uses index argument when provided", () => {
+  test('reducer uses index argument when provided', () => {
     expect([1, 2, 3].myFilter(isEvenIndex)).toStrictEqual([1, 3]);
     expect([-1, -3, 4, 99].myFilter(isOddIndex)).toStrictEqual([-3, 99]);
   });
 
-  test("reducer uses array argument when provided", () => {
+  test('reducer uses array argument when provided', () => {
     expect([1, 2, 3, 4].myFilter(isSquareEven)).toStrictEqual([2, 4]);
     expect([-3, 4, 1, 5].myFilter(isSquareOdd)).toStrictEqual([-3, 1, 5]);
   });
 
-  test("uses this argument", () => {
+  test('uses this argument', () => {
     expect([1, 2, 3, 4].myFilter(isThisProductEven)).toStrictEqual([]);
-    expect([1, 2, 3, 4].myFilter(isThisProductEven, 10)).toStrictEqual([
-      1, 2, 3, 4,
-    ]);
+    expect([1, 2, 3, 4].myFilter(isThisProductEven, 10)).toStrictEqual([1, 2, 3, 4]);
     expect([1, 2, 3, 4].myFilter(isThisProductEven, 9)).toStrictEqual([2, 4]);
     expect([1, 2, 3, 4].myFilter(isThisProductEvenArrowFn)).toStrictEqual([]);
-    expect([1, 2, 3, 4].myFilter(isThisProductEvenArrowFn, 10)).toStrictEqual(
-      [],
-    );
-    expect([1, 2, 3, 4].myFilter(isThisProductEvenArrowFn, 9)).toStrictEqual(
-      [],
-    );
+    expect([1, 2, 3, 4].myFilter(isThisProductEvenArrowFn, 10)).toStrictEqual([]);
+    expect([1, 2, 3, 4].myFilter(isThisProductEvenArrowFn, 9)).toStrictEqual([]);
   });
 
-  test("sparse arrays", () => {
+  test('sparse arrays', () => {
     expect([, , ,].myFilter(isEven)).toStrictEqual([]);
     expect([1, 2, , 4].myFilter(isEven)).toStrictEqual([2, 4]);
     expect([1, , 2, , 4, 7, 9].myFilter(isOdd)).toStrictEqual([1, 7, 9]);
--- exercises/typescript/array-prototype-filter/index.ts
+++ exercises/typescript/array-prototype-filter/index.ts
@@ -1,26 +1,10 @@
 interface Array<T> {
   myFilter(
     callbackFn: (value: T, index: number, array: Array<T>) => boolean,
-    thisArg?: any,
+    thisArg?: any
   ): Array<T>;
 }
 
 Array.prototype.myFilter = function (callbackFn, thisArg) {
-  const len = this.length;
-  const results = [];
-
-  // Snapshot the original range so callback-side pushes do not extend the walk.
-  for (let k = 0; k < len; k++) {
-    const kValue = this[k];
-    if (
-      // Native filter skips holes entirely instead of invoking the predicate
-      // with `undefined` for missing indexes.
-      Object.hasOwn(this, k) &&
-      callbackFn.call(thisArg, kValue, k, this)
-    ) {
-      results.push(kValue);
-    }
-  }
-
-  return results;
+  throw 'Not implemented!';
 };
