f81d716c4d548ef3de8688d618a35b6907299a9e
Oleg Bordun <oleg.bordun@outlook.com>
2026-05-27 19:06:24 +0200

    feat(promise-all): update README with usage examples and implementation details; add comprehensive tests for promiseAll function

--- exercises/typescript/promise-all/index.ts
+++ exercises/typescript/promise-all/index.ts
@@ -1,3 +1,7 @@
-export const promiseAll = () => {
-  // Your code here
-};
+type ReturnValue<T> = { -readonly [P in keyof T]: Awaited<T[P]> };
+
+export default function promiseAll<T extends readonly unknown[] | []>(
+  iterable: T
+): Promise<ReturnValue<T>> {
+  throw 'not implemented';
+}
